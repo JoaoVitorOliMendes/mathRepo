ccace9505b4b6a7492f5644dbdc81668440002b0
JoaoVitorOliMendes <joaozao.oli@gmail.com>
2023-08-10 22:03:35 -0300

    small gauss update; poland cauculator in C

--- gauss.py
+++ gauss.py
@@ -1,7 +1,7 @@
 matrix = [
-        [1, 1, 1],
-        [2, 1, 4],
-        [2, 3, 5]
+        [1, 1, 0],
+        [1, 1, 0],
+        [0, 0, 1]
         ]
 equals = [1000, 2000, 2500]
 
@@ -15,23 +15,19 @@ for rowIndex in range(matrixLen):
 
 for pivotRowIndex in range(matrixLen):
     pivot = matrix[pivotRowIndex][pivotRowIndex]
-    print('Pivot: ' + str(matrix[pivotRowIndex][pivotRowIndex]))
     for rowIndex in range(matrixLen):
-        print('=================')
         if rowIndex != pivotRowIndex:
-            print('Li: ' + str(rowIndex))
             quo = matrix[rowIndex][pivotRowIndex] / pivot
-            print('quo: ' + str(matrix[rowIndex][pivotRowIndex]/pivot))
             for keyIndex in range(len(matrix[rowIndex])):
                 sub = quo * matrix[pivotRowIndex][keyIndex]
-                print('Sub: ' + str(sub))
                 matrix[rowIndex][keyIndex] -= sub
-            print(equals[rowIndex])
             equals[rowIndex] = equals[rowIndex] - (quo * equals[pivotRowIndex])
-            print(equals[rowIndex])
-            print(matrix)
-            print(equals)
+
+for rowIndex in range(matrixLen):
+    pivot = matrix[rowIndex][rowIndex]
+    for keyIndex in range(len(matrix[rowIndex])):
+        matrix[rowIndex][keyIndex] = matrix[rowIndex][keyIndex] / pivot
+    equals[rowIndex] = round(equals[rowIndex] / pivot)
 
 print('Result')
 print(equals)
-print(matrix)
