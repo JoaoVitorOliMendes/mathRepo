276fc5827c826e6763ecf9f2fec6006bcbb96976
JoaoVitorOliMendes <joaovitordeoliveiramendes@tutanota.com>
2023-11-08 15:33:48 -0300

    todos

--- Simplex/main.py
+++ Simplex/main.py
@@ -4,11 +4,18 @@ from simplex.max import simplexMax
 from simplex.min import simplexMin
 
 
-maxZ = splitInt(input('Informe os valores de Max Z: \n'))
+maxZ = splitInt(input('Informe os valores de Max Z: \n')) # TROCAR MENSAGEM E NOME VARIAVEL
 inpX = splitInt(input('Informe a expressão 1:\n'))
 inpY = splitInt(input('Informe a expressão 2:\n'))
 inpZ = splitInt(input('Informe a expressão 3:\n'))
 
-matrix = makeMatrix(inpX, inpY, inpZ, -1)
-
+'''
+MIN
+matrix = makeMatrix(inpX, inpY, inpZ, -1) *** -1
 print(simplexMin(matrix, maxZ))
+'''
+'''
+MAX
+matrix = makeMatrix(inpX, inpY, inpZ, 1) *** -1
+print(simplexMax(matrix, maxZ))
+'''
